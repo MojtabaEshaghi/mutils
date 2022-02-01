c0c24fa91d8cce0a0bc145995fe0ff20816b841f
Mr.mojtaba <Mr.Mojtaba47@gmail.com>
2022-02-01 11:27:04 +0330

    test:
    write test font style

--- test/mutils_test.dart
+++ test/mutils_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mutils/mutils.dart';
@@ -8,5 +10,17 @@ void main() {
         Mut.appTheme(color:Color(0xff636987)),
         ThemeData(
             primarySwatch: Mut.generateMaterialColor(Color(0xff636987))));
+  });
+
+
+  test("test font",(){
+    expect(
+      Mut.mtStyle(),
+      TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.normal,
+        fontSize: 14.0
+      )
+    );
   });
 }
